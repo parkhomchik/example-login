ee378888f3e6ae1d87aafa4e27f2929bc21d1868
parkhomchik <parkhomchik@gmail.com>
2017-01-10 20:05:37 +0600

    add session manager

--- main.go
+++ main.go
@@ -27,6 +27,38 @@ const loginBlock = `<form action="/login" method="post">
     <input type="submit" value="Login">
 </form>`
 
+//SESSIONS
+
+type SessionManager struct {
+	Sessions map[string]string
+}
+
+func NewSessions() *SessionManager {
+	s := new(SessionManager)
+	s.Sessions = make(map[string]string)
+	return s
+}
+
+func (s *SessionManager) Add(userName string, sessionID string) {
+	if _, ok := s.Sessions[sessionID]; !ok {
+		s.Sessions[sessionID] = userName
+	}
+}
+
+func (s *SessionManager) GetUser(sessionID string) (User string) {
+	return s.Sessions[sessionID]
+}
+
+func (s *SessionManager) DeleteSession(sessionID string) {
+	if _, ok := s.Sessions[sessionID]; ok {
+		delete(s.Sessions, sessionID)
+	}
+}
+
+//SESSIONS END
+
+var manager = NewSessions()
+
 func main() {
 	http.HandleFunc("/index", index)
 	http.HandleFunc("/", index)
@@ -40,6 +72,14 @@ func main() {
 }
 
 func index(res http.ResponseWriter, req *http.Request) {
+	cookie, err := req.Cookie("sessionid")
+	var html string
+	if err == nil {
+		html = fmt.Sprintf(indexPage, "<a href='/logoff'> Logoff "+manager.GetUser(cookie.Value)+"</a>")
+	} else {
+		html = fmt.Sprintf(indexPage, loginBlock)
+	}
+
 	fmt.Println("index page")
 	res.Header().Set(
 		"Content-Type",
@@ -47,7 +87,7 @@ func index(res http.ResponseWriter, req *http.Request) {
 	)
 	io.WriteString(
 		res,
-		indexPage,
+		html,
 	)
 }
 
@@ -70,17 +110,22 @@ func login(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 
-		fmt.Println("username:", username)
-		fmt.Println("password:", password)
-
+		fmt.Println("username:", username, "password:", password)
+		sessionID := sessionID()
+		manager.Add(username, sessionID)
 		expiration := time.Now().Add(365 * 24 * time.Hour)
-		cookie := http.Cookie{Name: "sessionid", Value: sessionID(), Expires: expiration}
+		cookie := http.Cookie{Name: "sessionid", Value: sessionID, Expires: expiration}
 		http.SetCookie(w, &cookie)
 
-		http.Redirect(w, r, "/", 200)
+		http.Redirect(w, r, "/", 301)
+		return
 	}
 
 }
+
+/*func logoff(w http.ResponseWriter, r *http.Request) {
+
+}*/
 
 func sessionID() string {
 	b := make([]byte, 32)
